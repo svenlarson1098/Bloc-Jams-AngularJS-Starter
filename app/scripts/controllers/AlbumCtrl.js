2117138d26ef3edc4331ded6b3d4d3267d645efa
svenlarson1098 <svenlarson1098@gmail.com>
2017-12-23 06:23:54 -0800

    Lesson 6 Completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,11 @@
 (function() {
-  function AlbumCtrl() {
+  function AlbumCtrl(Fixtures) {
 
-    this.albumData = angular.copy(albumPicasso);
+    this.albumData = Fixtures.getAlbum();
   }
 
     angular
       .module('blocJams')
       .controller('AlbumCtrl', AlbumCtrl);
+      .controller('AlbumCtrl', ['Fitures', AlbumCtrl]);
 })();
